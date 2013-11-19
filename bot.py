46010979530c4ca745cf13aa488a126990bb2ae0
Peter Gicking <pgicking@gmail.com>
2013-11-18 17:36:32 -0800

    init commit

---
+++ bot.py
@@ -0,0 +1,34 @@
+#Peter Gicking, 11/18/13 
+#irc handle: swook
+
+
+import socket
+import sys
+
+server = 'iss.cat.pdx.edu'       #settings
+channel = '#swookbot'
+botnick = 'swookbot'
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the socket
+print('connecting to:'+server)
+irc.connect((server, 6667))                                                 #connects to the server
+irc.send(bytes('USER '+ botnick +' '+ botnick +' '+ botnick +' :Swooks Bot\n','UTF-8')) 	#user authentication
+irc.send(bytes('NICK '+ botnick +'\n','UTF-8'))                            #sets nick
+#irc.send("PRIVMSG nickserv :iNOOPE\r\n")    				#auth
+irc.send(bytes('JOIN '+ channel +'\n','UTF-8'))  							#join the chan
+irc.send(bytes('PRIVMSG #swookbot :Successfully joined the channel\r\n','UTF-8'))      				
+
+while 1:    				#puts it in a loop
+   text=irc.recv( 4096 ) 	#receive the text
+   print(text)   			#print text to console
+
+   if text.find(bytes('PING','UTF-8')) != -1:                          #check if 'PING' is found
+      irc.send(bytes('PONG \r\n', 'UTF-8'))
+	  #irc.send(bytes('PONG ' + text.split() [1] + '\r\n', 'UTF-8'))    #returns 'PONG' back to the server (prevents pinging out!
+
+	if text.find(bytes('?swookbot ping', 'UTF-8')) != -1:
+		irc.send(bytes('PRIVMSG #swookbot :pong', 'UTF-8'))
+	
+	if text.find(bytes('?swookbot quit', 'UTF-8' )) != -1:
+		irc.send(bytes('PRIVMSG #swookbot  :Quitting IRC', 'UTF-8'))
+		irc.send(bytes('QUIT\r\n')
